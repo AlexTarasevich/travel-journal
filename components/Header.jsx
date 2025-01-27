b54f8e5b101ee46b3c3d25324e7efd9d2c05ee6f
Aleksandr Tarasevich <tarasevich.alex01@mail.ru>
2025-01-27 16:47:22 +0300

    Add cards

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header>
+            <img src="../images/globe.png" alt="globe ion" />
+            <h1>My travel journal</h1>
+        </header>
+    )
+}
